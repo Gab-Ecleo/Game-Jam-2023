bb40490a4c722a90a93d54a0d50563ea45f2c46a
Kyle Leo Abelgas <kyleabelgas.taktyl@gmail.com>
2023-10-21 07:50:55 +0800

    Added Player Manager and Seed Interaction
    - Added player manager to get player object
    - Added seed interaction script
    - Added seed interaction prefab

---
+++ Game Jam 2023/Assets/Scripts/Seed/SeedInteraction.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeedInteraction : MonoBehaviour
+{
+    [SerializeField] private GameObject seedText;
+    [SerializeField] private KeyCode interactKey;
+    [SerializeField] private float detectionRange;
+
+    private GameObject player;
+    private bool isInteractable;
+
+    private void Start()
+    {
+        player = PlayerManager.Instance.GetPlayer();
+        isInteractable = false;
+    }
+
+    private void Update()
+    {
+        seedText.SetActive(false);
+
+        if (!isInteractable) return;
+
+        float distance = Vector2.Distance(transform.position, player.transform.position);
+
+        if (distance > detectionRange) return;
+
+        seedText.SetActive(true);
+
+        if (!Input.GetKeyDown(interactKey)) return;
+
+        //Add interaction here
+    }
+
+    public void EnableInteraction(bool state)
+    {
+        isInteractable = state;
+    }
+}
