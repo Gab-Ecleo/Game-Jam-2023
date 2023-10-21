5b79945e17c098fc5bfd22630d9decf82ac18351
Kyle Leo Abelgas <kyleabelgas.taktyl@gmail.com>
2023-10-22 00:34:57 +0800

    Manager Instantiator
    - Created a GameMaster to instantiate managers
    - Created manager prefabs

---
+++ Game Jam 2023/Assets/Game Managers/Game Master/GameMaster.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMaster : MonoBehaviour
+{
+    [SerializeField] private GameObject[] managerPrefabs;
+
+    private void Awake()
+    {
+        foreach(GameObject manager in managerPrefabs)
+        {
+            Instantiate(manager, transform);
+        }
+    }
+}
